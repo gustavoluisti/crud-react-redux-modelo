c5a88d53649dd1a2a424b9f772548af256b477f9
Luis Gustavo Santos <spartadev@spartas-MacBook-Pro.local>
2019-02-08 14:33:09 -0200

    Rotas

--- jwt/src/redux/actionCreators.js
+++ jwt/src/redux/actionCreators.js
@@ -10,6 +10,10 @@ export const {
 
     authRequest: null,
     authSuccess: ['user'],
-    authFailure: null
+    authFailure: null,
+
+    getRunsRequest: null,
+    getRunsSuccess: ['runs'],
+    getRunsFailure: null
 })
 export default Creators
---
+++ jwt/src/screens/Restrito/Runs.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Runs = props => <h1>Home</h1>
+
+export default Runs
