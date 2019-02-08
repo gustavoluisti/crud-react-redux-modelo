0f9994c5626ee546559039c612dcab31193600ac
Luis Gustavo Santos <spartadev@spartas-MacBook-Pro.local>
2019-02-08 15:31:44 -0200

    Reducers

--- jwt/src/redux/reducers/index.js
+++ jwt/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 
 import auth from './auth'
+import runs from './runs'
 
 const rootReducer = combineReducers({
-    auth: auth
+    auth,
+    runs
 })
 export default rootReducer
